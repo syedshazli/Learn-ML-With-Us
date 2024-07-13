d53714faca6bdc38d07a7a800561da9ade38ca23
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-13 10:36:37 -0400

    Create abduAssignmentOne.py

---
+++ Python Assignments/Assignment One/abduAssignmentOne.py
@@ -0,0 +1,26 @@
+import os
+import argparse
+
+# Step 1: Parse Command line arguments (there are 3 of them)
+parser = argparse.ArgumentParser(description='takes in file and outputs date of file, file size in MB, and name of file')
+parser.add_argument()
+parser.add_argument()
+parser.add_argument()
+
+args = parser.parse_args()
+
+# Step 2
+
+
+
+# Step 3
+
+
+# Step 4
+
+
+# Step 5
+
+
+
+# Step 6
