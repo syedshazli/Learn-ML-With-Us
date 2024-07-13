d528183640cd3de9d27945857a9c606dfd29be64
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-13 12:48:24 -0400

    Update abduAssignmentOne.py

--- Python Assignments/Assignment One/abduAssignmentOne.py
+++ Python Assignments/Assignment One/abduAssignmentOne.py
@@ -3,9 +3,9 @@ import argparse
 
 # Step 1: Parse Command line arguments (there are 3 of them)
 parser = argparse.ArgumentParser(description='takes in file and outputs date of file, file size in MB, and name of file')
-parser.add_argument()
-parser.add_argument()
-parser.add_argument()
+parser.add_argument(sizeFile)
+parser.add_argument(dateFile)
+parser.add_argument(nameFile)
 
 args = parser.parse_args()
 
