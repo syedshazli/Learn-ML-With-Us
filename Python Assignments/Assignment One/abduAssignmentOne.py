9c56bef854d6ee919aee1079d91e8cd65136b83f
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-14 11:06:45 -0400

    Update abduAssignmentOne.py

--- Python Assignments/Assignment One/abduAssignmentOne.py
+++ Python Assignments/Assignment One/abduAssignmentOne.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+import stat 
+import time
 import csv
 
 # Step 1: Parse Command line arguments (there are 3 of them)
@@ -22,16 +24,19 @@ def read_files(directory):
 def getFileAttributes(directory, path):
   name = []
   #for files in path:
-    name.append(files)
+    #name.append(files)
+  size_in_mb = stats.st_size / (1024 * 1024)
+  
 
 
   return 
   
   
   
-# Step 4:	Convert File Size: Convert the file size to megabytes (MB).
-  
 
 # Step 5:	Write to CSV: Use the csv module to write the file attributes to a CSV file.
+
+def writeCSV():
+  #use the 'w' for mode to write in a new csv file using the with open method
 
 # Step 6:	Error Checking and Help Display: The argparse module automatically provides error checking and help display.
