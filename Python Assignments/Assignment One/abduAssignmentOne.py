c0c7decb3772a43e1b52af64a10f8ebe3875ddb5
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-07-14 10:54:56 -0400

    Update abduAssignmentOne.py

--- Python Assignments/Assignment One/abduAssignmentOne.py
+++ Python Assignments/Assignment One/abduAssignmentOne.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import csv
 
 # Step 1: Parse Command line arguments (there are 3 of them)
 parser = argparse.ArgumentParser(description='takes in file and outputs date of file, file size in MB, and name of file')
@@ -12,14 +13,25 @@ args = parser.parse_args()
 # Step 2: Read Files from Directory: Use the os module to list files in a given directory.
 #input is directory
 def read_files(directory):
-  path = os.listdir(directory)
+  path = os.listdir(directory) #use listdir method which appends the files in a directory to a list variable "path"
   print("Files and directories in path", directory, ":")
   print(path)
+  return path
 
-# Step 3:	Get File Attributes: Retrieve file permissions, size, modification date, and name for each file.
+# Step 3:	Get File Attributes: Retrieve file size, modification date, and name for each file.  
+def getFileAttributes(directory, path):
+  name = []
+  #for files in path:
+    name.append(files)
 
+
+  return 
+  
+  
+  
 # Step 4:	Convert File Size: Convert the file size to megabytes (MB).
+  
 
-# Step 5:		Write to CSV: Use the csv module to write the file attributes to a CSV file.
+# Step 5:	Write to CSV: Use the csv module to write the file attributes to a CSV file.
 
 # Step 6:	Error Checking and Help Display: The argparse module automatically provides error checking and help display.
