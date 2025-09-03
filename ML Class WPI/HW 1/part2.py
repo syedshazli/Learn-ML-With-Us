db29b78568d17abcabb7309d8a72808e7b260664
syedshazli <syed.abdu1818@gmail.com>
2025-09-03 13:34:34 -0400

    FINISHED. GOAT

--- ML Class WPI/HW 1/part2.py
+++ ML Class WPI/HW 1/part2.py
@@ -1,3 +1,18 @@
 from part1 import *
 import numpy as np
 import sys
+
+def main():
+
+    X,Y = Tree.load_dataset('part2.csv')
+    model = Tree.train(X,Y)
+
+    predictionSet = np.array([['low','low','no','yes','male'],
+                              ['low','medium','yes','yes','female']])
+    predictionSet = predictionSet.T
+
+    outputPrediction = Tree.predict(model,predictionSet)
+    print(outputPrediction)
+
+if __name__ == "__main__":
+    main()
