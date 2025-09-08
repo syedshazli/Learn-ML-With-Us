4dc12e2847766bec621c59d5eef076c0ac992fd2
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2025-09-08 17:45:16 -0400

    Add files via upload

---
+++ ML Class WPI/HW 2/application.py
@@ -0,0 +1,28 @@
+import math
+import numpy as np
+from linear_regression import *
+from sklearn.datasets import make_regression
+# Note: please don't add any new package, you should solve this problem using only the packages above.
+#-------------------------------------------------------------------------
+'''
+    Problem 2: Apply your Linear Regression
+    In this problem, use your linear regression method implemented in problem 1 to do the prediction.
+    Play with parameters alpha and number of epoch to make sure your test loss is smaller than 1e-2.
+    Report your parameter, your train_loss and test_loss 
+    Note: please don't use any existing package for linear regression problem, use your own version.
+'''
+
+#--------------------------
+
+n_samples = 200
+X,y = make_regression(n_samples= n_samples, n_features=4, random_state=1)
+y = np.array(y).T
+X = np.array(X)
+Xtrain, Ytrain, Xtest, Ytest = X[::2], y[::2], X[1::2], y[1::2]
+
+#########################################
+## INSERT YOUR CODE HERE
+
+
+#########################################
+
